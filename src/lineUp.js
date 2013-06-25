87bf8c3656e04a45ec952dec2d7e12d12360ecf6
Hack Reactor Students <github@hackreactor.com>
2013-06-24 17:46:05 -0700

    made lineUp button

--- src/lineUp.js
+++ src/lineUp.js
@@ -1,3 +1,18 @@
-var lineUp = function(top, left, timeBetweenSteps){
+var newSetPosition = function(top, element){
+  /* Use css top and left properties to position our <span> tag
+   * where it belongs on the page. See http://api.jquery.com/css/
+   */
+  var styleSettings = {
+    top: top,
+    left: 0
+  };
+  element.$node.css(styleSettings);
+};
 
+var lineUp = function(array){
+  var top = 0;
+  for (var i = 0; i < array.length; i++){
+    top = top + 50;
+    newSetPosition(top, array[i]);
+  }
 };
